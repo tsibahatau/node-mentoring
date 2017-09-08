5adc707d25dd295e0c4bcf4f297d170b98f9eca8
Tsimur_Sibahatau <tsimur_sibahatau@epam.com>
2017-09-08 18:48:40 +0300

    npm 1 task

---
+++ index.js
@@ -0,0 +1,9 @@
+import config from './config/config';
+
+console.log(config.name);
+
+import User from './models/User';
+import Product from './models/Product';
+
+let nosense = new User();
+nosense = new Product();
